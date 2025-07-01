8a6206e165c79cb62037ea95f8a95f9d6721682b
Andrew Gazelka <drew@x.ai>
2025-07-01 15:03:39 -0700

    update JetBrains plugin

--- jetbrains-plugin/build.gradle.kts
+++ jetbrains-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm") version "1.9.25"
-  id("org.jetbrains.intellij") version "1.17.4"
+  id("org.jetbrains.intellij.platform") version "2.6.0"
 }
 
 group = "com.andrewgazelka"
@@ -9,42 +9,55 @@ version = "1.0-SNAPSHOT"
 
 repositories {
   mavenCentral()
-}
-
-// Configure Gradle IntelliJ Plugin
-intellij {
-  version.set("2023.2.6")
-  type.set("IC") // Target IDE Platform
-  plugins.set(listOf(/* Plugin Dependencies */))
+  
+  // IntelliJ Platform Plugin repositories
+  intellijPlatform {
+    defaultRepositories()
+  }
 }
 
 dependencies {
   implementation("org.java-websocket:Java-WebSocket:1.5.4")
   implementation("com.google.code.gson:gson:2.10.1")
+  
+  // IntelliJ Platform dependencies
+  intellijPlatform {
+    intellijIdeaCommunity("2024.2")
+    
+    pluginVerifier()
+    zipSigner()
+  }
+}
+
+// Configure IntelliJ Platform Plugin
+intellijPlatform {
+  pluginConfiguration {
+    version = project.version.toString()
+    
+    ideaVersion {
+      sinceBuild = "242"
+      untilBuild = "261.*"
+    }
+  }
+  
+  signing {
+    certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+    privateKey = providers.environmentVariable("PRIVATE_KEY")
+    password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+  }
+  
+  publishing {
+    token = providers.environmentVariable("PUBLISH_TOKEN")
+  }
 }
 
 tasks {
   // Set the JVM compatibility versions
   withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
   }
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
-
-  patchPluginXml {
-    sinceBuild.set("232")
-    untilBuild.set("242.*")
-  }
-
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
+    kotlinOptions.jvmTarget = "21"
   }
 }
